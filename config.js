561514bd8776acd5886ceb8b163b81aac0d37a69
Isuru <91410702+dayanisuru@users.noreply.github.com>
2023-02-05 14:53:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
   readmessage: process.env.READ_MESSAGE || false,
-  HANDLERS: process.env.PREFIX || ['.'],
+  HANDLERS: process.env.PREFIX || ['#','!','.'],
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
   levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
